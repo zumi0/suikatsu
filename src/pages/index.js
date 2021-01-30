4f24b024fe050b7d34e83c3faf2e4776e5cfdcf8
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2021-01-30 15:24:24 +0900

    add test

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,9 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
+      <div>
+        <p>test</p>
+      </div>
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
