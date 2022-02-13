7de7af1054e325bee95f72bbccabdf757208cce8
zumi0 <toshin.fish@gmail.com>
2022-02-13 16:24:53 +0900

    add map.vue to try how to use nuxt-leaflet

---
+++ pages/map.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="map-wrap" style="height: 100vh; width: 100%;">
+      <client-only>
+        <l-map :zoom="13" :center="[47.413220, -1.219482]">
+          <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+          <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
+        </l-map>
+      </client-only>
+    </div>
+  </div>
+</template>
+
+
+<script>
+//import aquariums from '@/assets/aquariums.json'
+
+export default {
+    data () {
+        return {
+            //aquariums: aquariums,
+            center: [36.207519, 139.664034],
+            tile_url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        }
+    }
+}
+</script>
