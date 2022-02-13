8bd350d7e7f37129ca21a892a2145336f5a9d5a2
zumi0 <toshin.fish@gmail.com>
2022-02-13 16:24:06 +0900

    add nuxt-leaflet components

--- plugins/leaflet.js
+++ plugins/leaflet.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
 import LControlFullscreen from 'vue2-leaflet-fullscreen'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 Vue.component('l-control-fullscreen', LControlFullscreen)
