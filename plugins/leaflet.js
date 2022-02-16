a29e5d6e8445069385c0f0dc1222d21c6aa6ea8d
zumi0 <toshin.fish@gmail.com>
2022-02-16 23:27:26 +0900

    add LControl component

--- plugins/leaflet.js
+++ plugins/leaflet.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import LControlFullscreen from 'vue2-leaflet-fullscreen'
-import { LMap, LTileLayer, LMarker, LIcon } from 'vue2-leaflet';
+import { LMap, LTileLayer, LMarker, LIcon, LControl } from 'vue2-leaflet';
 
 Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
 Vue.component('l-icon', LIcon);
+Vue.component('l-control', LControl);
 Vue.component('l-control-fullscreen', LControlFullscreen)
