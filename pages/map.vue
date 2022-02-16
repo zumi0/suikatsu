f19969e3d2c644545605a912f5630d153fdd069b
zumi0 <toshin.fish@gmail.com>
2022-02-16 22:34:37 +0900

    add marker where aquariums are

--- pages/map.vue
+++ pages/map.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <div id="map-wrap" style="height: 100vh; width: 100%;">
+    <div id="map-wrap">
       <client-only>
-        <l-map :zoom="13" :center="[47.413220, -1.219482]">
-          <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-          <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
+        <l-map :zoom="zoom" :center="center">
+          <l-tile-layer :url="tile_url" :attribution="attribution"></l-tile-layer>
+          <l-marker 
+            v-for="(value,key) in aquariums"
+            v-bind:key="key"
+            :lat-lng="[value.lat, value.lng]"
+            >
+            <l-popup>
+              <p><a :href="value.url" target="_blank">{{value.name}}</a></p>
+            </l-popup>
+          </l-marker>
         </l-map>
       </client-only>
     </div>
@@ -13,16 +21,28 @@
 
 
 <script>
-//import aquariums from '@/assets/aquariums.json'
+import aquariums from '@/assets/aquariums.json'
 
 export default {
     data () {
         return {
-            //aquariums: aquariums,
+            aquariums: aquariums,
+            test_icon_url: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+            blue_icon_url: '@/assets/fish_blue.png',
+            blue_icon_size: [64, 64],
+            zoom: 8,
             center: [36.207519, 139.664034],
             tile_url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-            attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
         }
     }
 }
 </script>
+
+<style scoped>
+#map-wrap {
+  position: fixed;
+  width: 100%;
+  height: calc(100% - 64px);
+}
+</style>
