cccc02df68ec063ac02d9c6ab5862d122b478879
zumi0 <toshin.fish@gmail.com>
2022-02-20 00:04:53 +0900

    change bulma card

--- components/Card.vue
+++ components/Card.vue
@@ -1,23 +1,18 @@
 <template>
-  <div class="column">
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title has-text-grey">
-          {{ title }}
-        </p>
-      </header>
-      <div class="card-content">
-        <div class="content has-text-centered">
-          <b-icon :icon="icon" size="is-large" type="is-primary" />
-        </div>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image">
+        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+        <a href="#">#css</a> <a href="#">#responsive</a>
+        <br>
+        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
       </div>
-      <footer class="card-footer">
-        <div class="card-footer-item">
-          <span>
-            <slot />
-          </span>
-        </div>
-      </footer>
     </div>
   </div>
 </template>
@@ -25,15 +20,5 @@
 <script>
 export default {
   name: 'BuefyCard',
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-    icon: {
-      type: String,
-      required: true,
-    },
-  },
 }
 </script>
