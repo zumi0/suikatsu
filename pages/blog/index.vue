138a96e95ed5261889d1b5ace70ea12bb1dc4f08
zumi0 <toshin.fish@gmail.com>
2022-02-19 20:39:25 +0900

    add blog list page

---
+++ pages/blog/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Description :title="title" :subtitle="subtitle" :description="description" />
+    </div>
+</template>
+
+<script>
+import Description from '~/components/Description'
+
+export default {
+    name: 'blog',
+    components: {
+      Description
+    },
+    data () {
+        return {
+            title: 'Blog',
+            subtitle: '水族館の記録一覧',
+            description: '管理人が行った水族館の記録を見ることができます。'
+        }
+    }
+}
