3780dbc24a664cd7d75b4e05a7bd20a0424669b1
zumi0 <toshin.fish@gmail.com>
2022-02-19 19:38:20 +0900

    add Buefy and delete Vuetify.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -38,8 +36,6 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -51,6 +47,10 @@ export default {
     // Simple usage
     'nuxt-leaflet',
     ['nuxt-leaflet', { /* module options */ }],
+    // Simple usage
+    'nuxt-buefy',
+    // Or you can customize
+    ['nuxt-buefy', { css: false, materialDesignIcons: false }],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -61,25 +61,6 @@ export default {
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
