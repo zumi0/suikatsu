e0b74f99dd131a80cbf2862d9bbb5995341c9a4f
zumi0 <toshin.fish@gmail.com>
2022-02-20 00:19:31 +0900

    add card components

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <section class="section">
-      <div class="columns is-mobile">
+      <b-image src="https://picsum.photos/600/400"/>
+      <Description :title="title_what" :subtitle="subtitle_what" :description="description_what" />
+      <Description :title="title_map" :subtitle="subtitle_map" />
+      <Card />
+      <Description :title="title_blog" :subtitle="subtitle_blog" />
+      <Card />g
+      <Description :title="title_about" :subtitle="subtitle_about" />
+      <!--
+      <div class="columns">
         <card title="Free" icon="github">
           Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
         </card>
@@ -19,6 +27,7 @@
           No other internal dependency
         </card>
       </div>
+      -->
     </section>
     <Footer />
   </div>
@@ -26,13 +35,28 @@
 
 <script>
 import Card from '~/components/Card'
+import Description from '~/components/Description'
 import Footer from '~/components/Footer'
 
 export default {
-  name: 'IndexPage',
-  components: {
-    Card,
-    Footer
-  },
+    name: 'IndexPage',
+    components: {
+      Card,
+      Footer,
+      Description
+    },
+    data () {
+      return {
+        title_what: 'What',
+        subtitle_what: 'これは何',
+        description_what: '管理人は水族館がとても好き',
+        title_map: 'Map',
+        subtitle_map: '日本の水族館一覧',
+        title_blog: 'Blog',
+        subtitle_blog: '水族館の記録一覧',
+        title_about: 'About Me',
+        subtitle_about: '管理人',
+      }
+    }
 }
 </script>
