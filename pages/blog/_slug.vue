22109044b3c58e0284bdb8a3184ad1fc3ff33fbb
zumi0 <toshin.fish@gmail.com>
2022-02-23 19:53:34 +0900

    add blog contents

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,21 @@
+	
+<template>
+  <article>
+    <h1>{{ article.title }}</h1>
+    <NuxtContent :document="article" />
+  </article>
+</template>
+ 
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const article = await $content('blog', params.slug).fetch()
+    return {
+      article
+    }
+  }
+}
+
+// memo
+// https://www.evoworx.co.jp/blog/nuxt-content-blog/
+</script>
