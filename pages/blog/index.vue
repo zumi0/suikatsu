2264d5e55b4b32b774c6065453bde2f5d6b7bed3
zumi0 <toshin.fish@gmail.com>
2022-02-23 19:54:27 +0900

    add blog list

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <Description :title="title" :subtitle="subtitle" :description="description" />
+        <Description title="Blog" subtitle="水族館の記録一覧" description="管理人が行った水族館の記録を見ることができます" />
+        <div>
+            <ul>
+                <li v-for="article in blog" :key="article.slug">
+                    <nuxt-link :to="'/blog/' + article.slug">
+                        <p>{{ article.title }}</p>
+                    </nuxt-link>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -10,13 +19,19 @@ import Description from '~/components/Description'
 export default {
     name: 'blog',
     components: {
-      Description
+      Description,
     },
     data () {
         return {
-            title: 'Blog',
-            subtitle: '水族館の記録一覧',
-            description: '管理人が行った水族館の記録を見ることができます。'
+        }
+    },
+    async asyncData ({ $content }) {
+        // 記事を全て取得（作成日で降順にソート）
+        const blog = await $content('blog').fetch()
+        return {
+        blog
         }
     }
 }
+
+</script>
